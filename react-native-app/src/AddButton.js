4795a36233d89c802552f03dca757b957260fffb
Rishichandra Wawhal <rishichandra.wawhal@gmail.com>
2018-05-17 16:51:17 +0530

    Updated HOCs to the new Query and Mutation components (#1)
    * Using apollo query components
    
    * Fixed a bug
    
    * Committing migrations

--- react-native-app/src/AddButton.js
+++ react-native-app/src/AddButton.js
@@ -1,38 +1,45 @@
 import React, { Component } from 'react';
 import {INSERT_TODO, FETCH_TODOS} from './queries';
-import { graphql } from 'react-apollo';
-import {Button} from 'react-native';
+import { graphql, Mutation } from 'react-apollo';
+import {Button, Keyboard} from 'react-native';
 
-export default graphql(INSERT_TODO)((props) => {
-
+const AddButton = (props) => {
   return (
-    <Button
-      title="Insert Todo"
-      style={props.style}
-      onPress={() => {
-        if (props.task.length === 0) { return; }
-        props.mutate({
-          variables: {
-            objects: [{
-              task: props.task,
-              completed: false,
-              user_id: props.userId
-            }]
-          },
-          update: (proxy, {data: {insert_todos}}) => {
-            const data = proxy.readQuery({ query: FETCH_TODOS});
-            const newTodo = {
-              task: props.task,
-              completed: false,
-              user_id: props.userId,
-              id: insert_todos.returning[0].id
-            }
-            data.todos.push(newTodo);
-            proxy.writeQuery({query: FETCH_TODOS, data})
-          }
-        });
-        props.handleTextChange('');
+    <Mutation
+      mutation={INSERT_TODO}
+      update= {(cache, {data: {insert_todos}}) => {
+        const data = cache.readQuery({ query: FETCH_TODOS});
+        const newTodo = {
+          task: props.task,
+          completed: false,
+          user_id: props.userId,
+          id: insert_todos.returning[0].id
+        }
+        data.todos.push(newTodo);
+        cache.writeQuery({query: FETCH_TODOS, data})
       }}
-    />
-  );
-});
+    >
+      {(insert_todos, {data}) => (
+        <Button
+          title="Insert Todo"
+          style={props.style}
+          onPress={() => {
+            insert_todos({
+              variables: {
+                objects: [{
+                  task: props.task,
+                  completed: false,
+                  user_id: props.userId
+                }]
+              }
+            });
+            props.handleTextChange("");
+            Keyboard.dismiss();
+          }}
+        />
+      )}
+    </Mutation>
+  )
+}
+
+export default AddButton;
